bb431703334bdde8fe25ba00538be1efab58c049
thomasarsouze <thomas.arsouze@zoho.com>
2023-04-19 16:25:25 +0200

    stats branch. JBD scripts.

--- notebook-openalea/jupyter_notebook_config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-# Configure nb_conda_kernels to avoid registering jupyter kernels in our conda
-# environment again.
-c.CondaKernelSpecManager.env_filter = f'.*envs/{os.environ["CONDA_ENV"]}.*'
